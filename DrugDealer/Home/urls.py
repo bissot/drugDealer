1481447c0aca08f4f8207763cf388ab4f022aa32
Tommaso-Valagussa98 <valat98@gmail.com>
2020-12-28 21:57:30 -0500

    added urls

--- DrugDealer/Home/urls.py
+++ DrugDealer/Home/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name = 'drugdealergame'),    # calls the function home in the views.py
     path('about/', views.about, name = 'aboutgame'),    # calls the function about in the views.py
     path('rules/', views.rules, name = 'rules'),
+    path('lobby/<str:gameroom_code>/', views.lobby, name = 'lobby'),	#calls the function lobby in views.py
+    path('game/<str:gameroom_code>/', views.game, name = 'game'),	#calls the function game in views.py
 ]
